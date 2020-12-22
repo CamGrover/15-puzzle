80d8c58992f5a121ee3c27f6b40f6189f8ab570b
Cameron Grover <camdgrover@gmail.com>
2020-12-22 09:37:00 -0600

    Removed duplicated code and now using import CPUSpinnerController

--- main.py
+++ main.py
@@ -4,23 +4,7 @@ from game_view import GameView
 from game_controller import GameController
 from constants import WIN_WIDTH, WIN_HEIGHT
 from events import TickEvent, EventManager, QuitEvent
-
-
-class CPUSpinnerController:
-    def __init__(self, ev_manager, clock):
-        self.ev_manager = ev_manager
-        self.clock = clock
-        self.keepGoing = True
-
-    def run(self):
-        while self.keepGoing:
-            self.clock.tick(60)
-            event = TickEvent()
-            self.ev_manager.post(event)
-
-    def notify(self, event, event_manager=None):
-        if isinstance(event, QuitEvent):
-            self.keepGoing = False
+from cpu_spinner_controller import CPUSpinnerController
 
 
 def main():
